b95c2b66a97e1bad0b48aaf22d3571a5da3a7889
boyo135 <baileyp7@icloud.com>
2024-08-15 15:06:08 +0100

    build: implement sass as alternative to css
    - install `svelte-preprocess`
    - install `sass`
    - add preprocess attribute to `svelte.config.js`

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
+import { sveltePreprocess } from "svelte-preprocess"
 import adapter from '@sveltejs/adapter-auto'
 
 const config = {
+	preprocess: [sveltePreprocess()],
 	kit: {
 		adapter: adapter()
 	}
