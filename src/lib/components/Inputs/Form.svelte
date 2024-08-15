ab9bb8241375bd942217a2f6c5b769fbdc008a55
boyo135 <baileyp7@icloud.com>
2024-08-15 14:30:57 +0100

    feat: create reusable `Form` component

---
+++ src/lib/components/Inputs/Form.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { enhance } from '$app/forms'
+
+    // Default to current route
+    export const action = "?"
+</script>
+
+<form method='POST' action={action} use:enhance>
+    <slot/>
+</form>
