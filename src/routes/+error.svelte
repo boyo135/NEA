5507dc8f9909340bd5ad97798d69c25047c0aaeb
boyo135 <baileyp7@icloud.com>
2024-08-15 14:32:31 +0100

    feat: create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from '$app/stores'
+</script>
+
+<h1>Congratulations! you've broken it :(</h1>
+{#if $page?.status}
+    <h3>{$page.status}: {$page.error.message}</h3>
+{/if}
