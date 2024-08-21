6a004f29251f5dff1ce814ad2a02cbf1fae1c530
boyo135 <baileyp7@icloud.com>
2024-08-21 17:18:00 +0100

    Chore: add various styling and improve login page

--- src/lib/components/Inputs/Form.svelte
+++ src/lib/components/Inputs/Form.svelte
@@ -5,6 +5,65 @@
     export const action = "?"
 </script>
 
-<form method='POST' action={action} use:enhance>
+<form method='POST' action={action} use:enhance {...$$restProps}>
+    <div class="inputs">
+        <slot name="inputs"/>
+    </div>
     <slot/>
 </form>
+
+<style lang="scss">
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        >.inputs {
+            :global(input + input) {
+                margin-top: 1rem;
+            }
+        }
+
+        :global(input) {
+            min-width: 32ch;
+            width: 100%;
+        }
+
+        :global(button) {
+            background: none;
+            border: none;
+            margin: 0;
+            padding: 0;
+
+            :global(p) {
+                margin: 0;
+                padding: 0;
+            }
+        }
+
+        :global(.button-pill),
+        :global(.button-pill.alt) {
+            padding: 0.5rem;
+        }
+
+        :global(.button-pill) {
+            border-radius: 1000000px;
+            background-color: var(--cta-1);
+            :global(p) {
+                color: #ffffff;
+            }
+        }
+        :global(.button-pill.alt) {
+            background-color: var(--cta-2);
+            :global(p) {
+                color: var(--cta-1);
+            }
+        }
+
+        :global(.button-slim) {
+            :global(p) {
+                color: var(--cta-1)
+            }
+        }
+    }
+</style>
---
+++ src/routes/(main)/login/+page@.svelte
@@ -0,0 +1,96 @@
+<script>
+    import { page } from '$app/stores'
+    import Form from '$lib/components/Inputs/Form.svelte'
+
+    $: FormResponse = $page.form
+    $: {
+        if (FormResponse) {
+            console.log(JSON.stringify(FormResponse, null, 4))
+        }
+    }
+
+    import healthyFoodSrc from '$lib/assets/images/healthyFood.png'
+</script>
+
+<div class="container__loginPage">
+    <h1 class="loginPage__title">Login</h1>
+    <Form class="loginPage__form">
+        <svelte:fragment slot="inputs">
+            <input name="username" id="username" type="text"
+                placeholder="Username"
+            >
+            <input name="password" id="password" type="password"
+                placeholder="Password"
+            >
+            <button class="button-slim" type="button">
+                <p class="fontW500">Forgot Password?</p>
+            </button>
+        </svelte:fragment>
+        <button class="button-pill" type="submit">
+            <p class="fontW700">Login</p>
+        </button>
+        <button class="button-pill alt" type="button">
+            <p class="fontW700">Register</p>
+        </button>
+    </Form>
+    <div class="loginPage__container__image">
+        <img class="loginPage__image" src={healthyFoodSrc} alt="Fruit"/>
+    </div>
+</div>
+
+<style lang="scss">
+
+    .container__loginPage {
+        width: 100vw;
+
+        height: 100vh;
+        height: 100dvh;
+
+        display: grid;
+        grid-template-columns: minmax(auto, 1fr) minmax(50ch, 1fr);
+        grid-template-rows: 1fr 1.618fr;
+        grid-template-areas:
+            "title image"
+            "form image";
+
+        // TODO: MAKE MORE RESPONSIVE
+        overflow: hidden;
+
+
+        >.loginPage__title {
+            grid-area: title;
+
+            place-content: center;
+            text-align: center;
+
+            padding: 1rem;
+            padding-bottom: 0;
+        }
+
+        >:global(.loginPage__form) {
+            grid-area: form;
+
+            justify-self: center;
+            justify-content: baseline;
+
+            padding: 1rem;
+
+            max-width: 40ch;
+        }
+
+        >.loginPage__container__image {
+            grid-area: image;
+
+            overflow: hidden;
+            position: relative;
+
+            >.loginPage__image {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: left center;
+            }
+        }
+    }
+
+</style>
