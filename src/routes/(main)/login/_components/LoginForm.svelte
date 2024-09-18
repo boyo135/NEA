58e73a15e25f4f693617f1d13828bdfade8e725c
boyo135 <baileyp7@icloud.com>
2024-09-18 14:53:34 +0100

    feat: created components for login and register forms

---
+++ src/routes/(main)/login/_components/LoginForm.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Form from '$lib/components/Inputs/Form.svelte'
+</script>
+
+
+
+<Form class="loginPage__form" action="?/login">
+    <svelte:fragment slot="inputs">
+        <input class="fontS100" name="username" id="username" type="text"
+            placeholder="Username"
+        >
+        <input class="fontS100" name="password" id="password" type="password"
+            placeholder="Password"
+        >
+        <button class="button-slim" type="button">
+            <p class="fontW500 fontS100">Forgot Password?</p>
+        </button>
+    </svelte:fragment>
+    <button class="button-pill" type="submit">
+        <p class="fontW700 fontS100">Login</p>
+    </button>
+    <a class="button button-pill alt" href="/login?type=register">
+        <p class="fontW700 fontS100">Create Account</p>
+    </a>
+</Form>
---
+++ src/routes/(main)/login/_components/RegisterForm.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Form from '$lib/components/Inputs/Form.svelte'
+</script>
+
+
+
+<Form class="loginPage__form" action="?/register">
+    <svelte:fragment slot="inputs">
+        <input class="fontS100" name="username" id="username" type="text"
+            placeholder="Username"
+        >
+        <input class="fontS100" name="email" id="email" type="text"
+            placeholder="Email"
+        >
+        <input class="fontS100" name="password" id="password" type="password"
+            placeholder="Password"
+        >
+        <input class="fontS100" name="confirmPassword" id="confirmPassword" type="password"
+            placeholder="Confirm Password"
+        >
+    </svelte:fragment>
+    <button class="button-pill" type="submit">
+        <p class="fontW700 fontS100">Register</p>
+    </button>
+    <a class="button button-pill alt" href="/login?type=login">
+        <p class="fontW700 fontS100">I Have An Account</p>
+    </a>
+</Form>
