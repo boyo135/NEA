67f2f8ebcb24917f5aec2e5ee64cc0e5b6b3dca1
boyo135 <baileyp7@icloud.com>
2024-10-11 12:41:48 +0100

    docs: add comments to dbOps

--- database/dbOps.js
+++ database/dbOps.js
@@ -6,7 +6,9 @@ import { dbClient, tables } from './dbUtils.js'
 
 // #region Operations
 const createUser = async (fieldValues) => {
+    // search the every entry in the database
     const matchingUsers = await dbClient.query.user.findMany({
+        //check if username OR email matches
         where: (user, { or, eq }) => or(
             eq(user.username, fieldValues.username),
             eq(user.email, fieldValues.email)
@@ -15,19 +17,26 @@ const createUser = async (fieldValues) => {
 
     const matchingFields = []
     for (const match of matchingUsers) {
+        // check if the username in the database matches the username inputted on the form
         if (match.username === fieldValues.username) {
+            // if it does, 'push' the username into the matching fields array
             matchingFields.push('username')
         }
+        // check if the email in the database matches the username inputted on the form
         if (match.email === fieldValues.email) {
+            // if it does, 'push' the email into the matching fields array
             matchingFields.push('email')
         }
     }
+    // check if there is anything in the matching fields array
     if (matchingFields.length > 0) return {
+        // if there is, return the error and the fields that don't match
         error: 'Unique constraint failed',
         fields: matchingFields
     }
-
+    // if there are no matching fields add the user to the database
     const user = (await dbClient.insert(tables.user).values(fieldValues).returning())[0]
+    // return the user and the fact that they have been added is a success 
     return {
         user: user,
         success: true
@@ -43,9 +52,10 @@ const getUser = async (fieldFilters) => {
             etc...
         }
     */
-    
+     // find the first user
     const user = await dbClient.query.user.findFirst({
         where: (user, { and, eq }) => and(
+            // where the field filters match
             ...Object.entries(fieldFilters).map(([key, value]) => {
                 return eq(user[key], value)
             })
